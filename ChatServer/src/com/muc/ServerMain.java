a06884a07f529af2f2173f212ac095c694c1d6b3
Perry Karlsen <per@sveinkarlsen.com>
2019-04-29 12:27:40 -0600

    Adding working timer of one second on the server and added so multiple people can join at the same time. The timer does not go at the same time yet.

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -31,7 +31,6 @@ public class ServerMain {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
-            System.out.println("sup");
         }
 
     }
